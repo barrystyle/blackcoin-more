a5f22ddf9aef9718b73cce714f0bfc3ffba4cdc6
Michel van Kessel <michelvankessel@protonmail.com>
2022-05-10 21:05:13 +0200

    default txindex

--- src/validation.h
+++ src/validation.h
@@ -84,7 +84,7 @@ static const int MAX_SCRIPTCHECK_THREADS = 15;
 static const int DEFAULT_SCRIPTCHECK_THREADS = 0;
 static const int64_t DEFAULT_MAX_TIP_AGE = 24 * 60 * 60;
 static const bool DEFAULT_CHECKPOINTS_ENABLED = false;
-static const bool DEFAULT_TXINDEX = false;
+static const bool DEFAULT_TXINDEX = true;
 static constexpr bool DEFAULT_COINSTATSINDEX{false};
 static const char* const DEFAULT_BLOCKFILTERINDEX = "0";
 /** Default for -persistmempool */
