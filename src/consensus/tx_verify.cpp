a4bf5565aac865d7583ec31c625d2f1ee32be164
lateminer <9951982+lateminer@users.noreply.github.com>
2022-05-18 16:05:27 +0300

    Use uint32_t for nTimeTx

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -167,7 +167,7 @@ int64_t GetTransactionSigOpCost(const CTransaction& tx, const CCoinsViewCache& i
     return nSigOps;
 }
 
-bool Consensus::CheckTxInputs(const CTransaction& tx, TxValidationState& state, const CCoinsViewCache& inputs, int nSpendHeight, CAmount& txfee, unsigned int nTimeTx)
+bool Consensus::CheckTxInputs(const CTransaction& tx, TxValidationState& state, const CCoinsViewCache& inputs, int nSpendHeight, CAmount& txfee, uint32_t nTimeTx)
 {
     // are the actual inputs available?
     if (!inputs.HaveInputs(tx)) {
@@ -223,7 +223,7 @@ bool Consensus::CheckTxInputs(const CTransaction& tx, TxValidationState& state, 
 }
 
 // Blackcoin: GetMinFee
-CAmount GetMinFee(const CTransaction& tx, unsigned int nTimeTx)
+CAmount GetMinFee(const CTransaction& tx, uint32_t nTimeTx)
 {
     size_t nBytes = ::GetSerializeSize(tx, PROTOCOL_VERSION);
     return GetMinFee(nBytes, nTimeTx);
